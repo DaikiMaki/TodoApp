af634fc848703a29cd3db083ff71df84773b034f
makidaiki9180@gmail.com <makidaiki9180@gmail.com>
2022-08-12 10:29:51 +0800

    ReactのインストールとReact Routerの設定

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -12,7 +12,7 @@ class TaskTest extends TestCase
     /**
      * @test
      */
-    public function 一覧を取得()
+    public function 一覧を取得できる()
     {
         $tasks = Task::factory()->count(10)->create();
 
@@ -22,4 +22,86 @@ class TaskTest extends TestCase
             ->assertOk()
             ->assertJsonCount($tasks->count());
     }
+
+    /**
+     * @test
+     */
+    public function 登録することができる()
+    {
+        $data = [
+            'title' => 'テスト投稿'
+        ];
+
+        $response = $this->postJson('api/tasks', $data);
+        
+        $response
+            ->assertCreated()
+            ->assertJsonFragment($data);
+    }
+    
+    /**
+     * @test
+     */
+    public function タイトルが空の場合は登録できない()
+    {
+        $data = [
+            'title' => ''
+        ];
+
+        $response = $this->postJson('api/tasks', $data);
+        $response
+            ->assertStatus(422)
+            ->assertJsonValidationErrors([
+                'title' => 'タイトルは必ず指定してください。'
+                ]);
+    }
+
+    /**
+     * @test
+     */
+    public function タイトルが256文字以上の場合は登録できない()
+    {
+        $data = [
+            'title' => str_repeat('あ', 256)
+        ];
+
+        $response = $this->postJson('api/tasks', $data);
+        $response
+            ->assertStatus(422)
+            ->assertJsonValidationErrors([
+                'title' => 'タイトルは、255文字以下で指定してください。'
+                ]);
+    }
+
+
+    /**
+     * @test
+     */
+    public function 更新することができる()
+    {
+        $task = Task::factory()->create();
+
+        $task->title = '書き換え';
+
+        $response = $this->patchJson("api/tasks/{$task->id}", $task->toArray());
+
+        $response
+            ->assertOk()
+            ->assertJsonFragment($task->toArray());
+    }
+
+    /**
+     * @test
+     */
+    public function 削除することができる()
+    {
+        $tasks = Task::factory()->count(10)->create();
+
+        $response = $this->deleteJson("api/tasks/1");
+        $response->assertOk();
+
+        $response = $this->getJson("api/tasks");
+        $response->assertJsonCount($tasks->count() - 1);
+    }
+
 }
